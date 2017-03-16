f66c5e8cad6759c47c793c5985acdd2889d90973
Ryan Gross <ryan@Ryans-MacBook-Pro.local>
2017-03-16 16:55:00 -0400

    Added unit testing for controllers

---
+++ draughts-model/src/test/java/stg/controller/CheckersGameController.java
@@ -0,0 +1,16 @@
+package stg.Controller;
+
+import org.junit.Before;
+
+/**
+ * Created by ryan on 3/16/17.
+ */
+public class CheckersGameController {
+
+    CheckersGameController checkersGameController;
+
+    @Before
+    public void setUp() {
+        checkersGameController = new CheckersGameController();
+    }
+}
